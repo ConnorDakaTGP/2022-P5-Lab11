a43731bd7eeea8114da39c35204edf5af4db4474
Connor Donegan <connor.donegan@vtsd.com>
2023-01-05 12:45:21 -0500

    Lab11

--- Lab11avst.java
+++ Lab11avst.java
@@ -13,25 +13,40 @@ public class Lab11avst
         // This main method needs additions for the 100 point version.
         Scanner input = new Scanner(System.in);
         final int MAX = 100;
-        boolean primes[];
+        boolean[] primes;
         primes = new boolean[MAX];
         computePrimes(primes);
         displayPrimes(primes);
     }
 
-    public static void computePrimes(boolean primes[])
+    public static void computePrimes(boolean[] primes)
     {
         System.out.println("\nCOMPUTING PRIME NUMBERS");
+        for(int k=2; k<primes.length; k++){
+            primes[k]=true;
 
+        }
+        for(int l = 2; l<primes.length;l++){
+            for(int k=l * 2; k<primes.length; k+=l) {
+            primes[k] = false;
+            }
+        }
     }
 
-    public static void displayPrimes(boolean primes[])
+    public static void displayPrimes(boolean[] primes)
     {
         System.out.println("\n\nPRIMES BETWEEN 1 AND "+ primes.length);
         System.out.println();
 
+        for(int k=2; k<primes.length; k++){
+            if(primes[k]){
+                System.out.print(k+" ");
+            }
+            else{
+                System.out.print("");
+            }
+        }
     }
-
 }
 
 
